1ef4c9f2c2c0e4deb6681917625bd6169c2dd84e
Controlador de Inventarios <verificadorsps@cafemaya.co>
2026-04-13 11:01:01 -0600

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,7 +2,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.38.0/+esm';
 
 const SUPABASE_URL = 'https://czxzghhwgfciyrgmgtai.supabase.co';
-const SUPABASE_KEY = 'PON_AQUI_TU_ANON_KEY';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6eHpnaGh3Z2ZjaXlyZ21ndGFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxNzYxMTgsImV4cCI6MjA4ODc1MjExOH0.LdODWE1RDggRq11NkfgFTUVv6g1E5iQxTfOtZnV0gF0';
 
 // Cliente principal
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
